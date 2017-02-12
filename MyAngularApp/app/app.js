fffb16f3293644d5696b3f916460f85ec5a9ab41
Adam Pridmore <adam_pridmore@hotmail.com>
2017-02-12 17:25:46 +0000

    - Added people view.  - Added menu to views.

--- MyAngularApp/app/app.js
+++ MyAngularApp/app/app.js
@@ -19,13 +19,19 @@
     ]);
 
     myApp.config([
-        '$locationProvider','$routeProvider',
-        function ($locationProvider, $routeProvider) {
+        '$locationProvider', '$routeProvider',
+        function($locationProvider, $routeProvider) {
             $locationProvider.html5Mode(true);
             $routeProvider.when('/View1',
                 {
                     templateUrl: '/app/views/View1/view1-template.html',
                     controller: 'view1Controller',
+                    controllerAs: 'vm'
+                })
+                .when('/People',
+                {
+                    templateUrl: '/app/views/People/people-template.html',
+                    controller: 'peopleController',
                     controllerAs: 'vm'
                 })
                 .otherwise({
---
+++ MyAngularApp/app/views/People/people.js
@@ -0,0 +1,17 @@
+﻿(function() {
+        angular.module("myApp")
+            .controller("peopleController",
+            [
+                "$http", function($http) {
+                    var vm = this;
+
+                    var url = "/Api/MyAppApi/123";
+                    $http
+                        .get(url)
+                        .then(function(responce) {
+                            console.log(responce.data);
+                            vm.people = responce.data;
+                        });
+                }
+            ]);
+    })();
