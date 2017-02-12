49e3c6531505d19119fd602d011012ad4c147fb4
Adam Pridmore <adam_pridmore@hotmail.com>
2017-02-12 13:24:35 +0000

    - Added basic angular app.

---
+++ MyAngularApp/Controllers/MyAppApiController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using System.Web.Http.Results;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace MyAngularApp.Controllers
+{
+    public class MyAppApiController : ApiController
+    {
+        [HttpGet]
+        public JsonResult<Person> Get(string id)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            return Json(new Person
+            {
+                Name = "Dave",
+                Age = 31
+            },settings);
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ MyAngularApp/app/app.js
@@ -0,0 +1,12 @@
+﻿angular
+    .module('myApp', [])
+    .controller('myAppController', ["$scope", Controller]);
+
+function Controller($scope) {
+    $scope.people = [
+        {
+            name: "Sally",
+            age: "35"
+        }
+    ];
+}
