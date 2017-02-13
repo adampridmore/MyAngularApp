08b222a0ab34d6aef78d0c0839a6b598980fd326
Adam Pridmore <adam_pridmore@hotmail.com>
2017-02-13 12:49:39 +0000

    - Extracted menu to a component.

---
+++ MyAngularApp/app/components/appMenu/appMenu.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    var menuController = function () {
+        var vm = this;
+        vm.menuItems = ["View1", "View2", "People"];
+    }
+
+    angular.module("myApp")
+        .component("appMenu",
+        {
+            templateUrl: "/app/components/appMenu/appMenu-template.html",
+            controller: ["$http", menuController],
+            controllerAs : "vm",
+            bindings: {
+                people: "="
+            }
+        });
+})();
